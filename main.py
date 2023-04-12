34cce84ca8b4c578c672caf806e68e59dff83516
AidenVu1 <100857445+AidenVu1@users.noreply.github.com>
2023-04-12 14:00:13 +0000

    finished

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# Ask the user for input
+base1 = float(input("Enter the length of base 1: "))
+base2 = float(input("Enter the length of base 2: "))
+height = float(input("Enter the height: "))
 
+# Calculate the area of the trapezoid
+area = 0.5 * (base1 + base2) * height
+
+# Print the result
+print("The area of the trapezoid is:", area)
